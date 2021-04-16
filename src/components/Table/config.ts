317c7a1983ff411d699b2590a2f315778cfaae94
JY <jherkyesse@gmail.com>
2021-04-16 17:52:23 +0800

    Create config.ts

---
+++ src/components/Table/config.ts
@@ -0,0 +1,15 @@
+export const tableClassName =
+  'user-select-none table-layout-fixed border-0 outline-none border-spacing-0 border-collapse-separate cursor-cell';
+export const tableTheadClassName =
+  'border-b-1 border-gray-300';
+export const tableTheadTrClassName = 'last:border-r-0';
+export const tableTheadThClassName =
+  'sticky h-24px top-0 border-b border-r p-1 bg-gray-300';
+export const tableTbodyClassName = 'w-full';
+export const tableTbodyTrClassName = 'last:border-r-0';
+export const tableTbodyTdClassName =
+  'relative border-r border-b whitespace-nowrap px-5px py-3px leading-5';
+
+export const stickyClassName = 'sticky left-0 bg-white z-10';
+
+export const cellHeight = 24;
