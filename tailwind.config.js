a4f6aaa368cf27423f4ae4a3ea35ebad8b5f3e65
JY <jherkyesse@gmail.com>
2021-04-29 19:00:53 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const colors = require('tailwindcss/colors');
 const plugin = require('tailwindcss/plugin');
 const pluginImportant = require('tailwindcss-important');
+const pluginPseudo = require('tailwindcss-pseudo-elements');
 const { omit, flatMap } = require('lodash');
 const flattenColorPalette = require('tailwindcss/lib/util/flattenColorPalette')
   .default;
@@ -13,19 +14,6 @@ module.exports = {
       ...colors,
     },
     extend: {
-      padding: {
-        '1px': '1px',
-        '2px': '2px',
-        '3px': '3px',
-        '4px': '4px',
-        '5px': '5px',
-        '6px': '6px',
-        '10px': '10px',
-        '15px': '15px',
-        '20px': '20px',
-        '25px': '25px',
-        '30px': '30px',
-      },
       margin: {
         '1px': '1px',
         '2px': '2px',
@@ -50,6 +38,38 @@ module.exports = {
         '-25px': '-25px',
         '-30px': '-30px',
       },
+      minHeight: {
+        '1/4': '25%',
+        '1/3': '33.333333%',
+        '1/2': '50%',
+        '3/4': '75%',
+      },
+      minWidth: {
+        '1/4': '25%',
+        '1/3': '33.333333%',
+        '1/2': '50%',
+        '3/4': '75%',
+      },
+      padding: {
+        '1px': '1px',
+        '2px': '2px',
+        '3px': '3px',
+        '4px': '4px',
+        '5px': '5px',
+        '6px': '6px',
+        '10px': '10px',
+        '15px': '15px',
+        '20px': '20px',
+        '25px': '25px',
+        '30px': '30px',
+      },
+      width: {
+        '100px': '100px',
+        '150px': '150px',
+        '200px': '200px',
+        '250px': '250px',
+        '300px': '300px',
+      },
       zIndex: {
         1: 1,
         33: 33,
@@ -64,15 +84,20 @@ module.exports = {
   },
   variants: {
     extend: {
-      backgroundColor: ['active', 'disabled', 'important'],
-      borderRadius: ['first', 'last', 'not', 'child'],
-      borderWidth: ['child', 'not-first', 'not-last'],
-      padding: ['child', 'first'],
-      margin: ['child', 'first', 'not-first', 'last'],
+      backgroundColor: ['active', 'disabled', 'important', 'before', 'after'],
+      borderRadius: ['first', 'last', 'not', 'child', 'important'],
+      borderWidth: ['child', 'not-first', 'not-last', 'before', 'after'],
+      borderColor: ['important'],
+      boxShadow: ['active'],
+      cursor: ['disabled'],
+      flex: ['child'],
+      margin: ['child', 'first', 'not-first', 'last', 'important'],
       overflow: ['important'],
       opacity: ['disabled', 'dark'],
-      cursor: ['disabled'],
+      padding: ['child', 'first', 'last', 'important'],
       pointerEvents: ['disabled'],
+      textColor: ['important'],
+      fontSize: ['important'],
     },
   },
   plugins: [
@@ -134,5 +159,6 @@ module.exports = {
       addUtilities(utilities);
     }),
     pluginImportant(),
+    pluginPseudo(),
   ],
 };
