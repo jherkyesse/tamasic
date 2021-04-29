9a8e8bced775bbc922fe2d6faf805f3c65bed74f
JY <jherkyesse@gmail.com>
2021-04-29 18:52:06 +0800

    Update index.tsx

--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -1,119 +1,164 @@
-import React, { useState, useEffect } from 'react';
-import { ScrollSync, AutoSizer } from 'react-virtualized';
+import React, { useState, useMemo, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { ScrollSync, AutoSizer, CellMeasurerCache } from 'react-virtualized';
 import { GridProvider } from './GridContext';
 import StickyGridHeader from './StickyGridHeader';
 import StickyGridBody from './StickyGridBody';
 import GridHeader from './GridHeader';
 import GridBody from './GridBody';
-import { cache, cellHeight } from './config';
+import { cellHeight, columnWidth } from './config';
+import { GridCellProps } from './type';
 
 const defaultGridClassName =
-  'w-full flex flex-nowrap border border-gray-300 overflow-hidden';
+  'w-full flex flex-nowrap border border-gray-300 overflow-hidden select-none';
 
 type GridProps = {
-  data: Array<{ index: number }>;
-  headerList: Array<any>;
-  headerKeyList: Array<string>;
-  height: number;
+  data: Array<GridCellProps>;
+  filterable: boolean;
+  headerList: Array<Array<GridCellProps>>;
+  headerKeyList: Array<Array<string>>;
+  height?: number;
   onChange?: Function;
+  overscanColumnCount?: number;
   selectable?: boolean;
-  stickyHeaderList?: Array<any>;
-  stickyHeaderKeyList?: Array<string>;
+  stickyHeaderList: Array<Array<GridCellProps>>;
+  stickyHeaderKeyList: Array<Array<string>>;
 };
 
 function Grid({
   data,
-  headerList = [],
-  headerKeyList = [],
-  height = 500,
+  filterable,
+  headerList,
+  headerKeyList,
+  height,
   onChange,
-  selectable = false,
-  stickyHeaderList = [],
-  stickyHeaderKeyList = [],
+  overscanColumnCount,
+  selectable,
+  stickyHeaderList,
+  stickyHeaderKeyList,
 }: GridProps) {
   const rowCount = data.length;
-  const columnCount = headerKeyList.length;
+  const columnCount = headerKeyList[0].length;
   const headerRowCount = headerList.length;
   const columnPropsList = headerList[headerRowCount - 1];
-  const stickyColumnCount = stickyHeaderKeyList.length;
+  const columnKeyList = headerKeyList[headerRowCount - 1];
+  const stickyColumnCount = stickyHeaderKeyList[0].length;
   const stickyHeaderRowCount = stickyHeaderList.length;
   const stickyColumnPropsList = stickyHeaderList[stickyHeaderRowCount - 1];
-  const stickyWidth = stickyHeaderList[0].reduce(
-    (acc, cur) => (acc ?? 100) + (cur.width ?? 100),
-    0,
+  const stickyColumnKeyList = stickyHeaderKeyList[headerRowCount - 1];
+
+  const stickyWidth = useMemo(
+    () =>
+      stickyHeaderList[stickyHeaderList.length - 1].reduce(
+        (acc: number, cur: { width?: number }) =>
+          (acc ?? 100) + (cur.width ?? 100),
+        0,
+      ),
+    [stickyHeaderList],
+  );
+  const allColumnKeyList = useMemo(
+    () => stickyColumnKeyList.concat(columnKeyList),
+    [columnKeyList, stickyColumnKeyList],
+  );
+  const readOnly = !onChange;
+  const [filterList, setFilterList] = useState(
+    new Array(allColumnKeyList.length).fill(''),
   );
 
-  const getColumnWidth = ({ index }) =>
-    columnPropsList.map(({ width = 100 }) => width)[index];
-  const getHeaderRowWidth = ({ index }) =>
-    headerList.map((_) => cellHeight)[index];
-  const getStickyColumnWidth = ({ index }) =>
-    stickyColumnPropsList.map(({ width = 100 }) => width)[index];
+  const filterData = useMemo(() => {
+    const filterData = data.reduce((acc: Array<object>, item: object) => {
+      const filterKey = filterList.every(
+        (filter: string, index: number) =>
+          !filter ||
+          (
+            item[allColumnKeyList[index]]?.changeValue ??
+            (item[allColumnKeyList[index]]?.value || '')
+          )
+            .toLowerCase()
+            .includes(filter.toLowerCase()),
+      );
+      if (filterKey) acc.push(item);
+      return acc;
+    }, []);
+    // if (orderKey) {
+    //   filterData.sort((a, b) =>
+    //     orderType === 'ascend'
+    //       ? (a[orderKey] || '').localeCompare(b[orderKey] || '')
+    //       : (b[orderKey] || '').localeCompare(a[orderKey] || ''),
+    //   );
+    // }
+    return filterData;
+  }, [data, filterList, allColumnKeyList]);
 
+  const onChangeFilterList = (value: string, columnIndex: number) => {
+    if (!setFilterList) return;
+    const newData = [...filterList];
+    newData[columnIndex] = value;
+    setFilterList(newData);
+  };
+  const getColumnWidth = ({ index }: { index: number }) =>
+    columnPropsList.map(({ width = 100 }) => width)[index];
+  const getStickyColumnWidth = ({ index }: { index: number }) =>
+    stickyColumnPropsList.map(({ width = 100 }) => width)[index];
   return (
     <GridProvider
       value={{
-        cache,
+        cache: new CellMeasurerCache({
+          defaultWidth: columnWidth,
+          defaultHeight: cellHeight,
+          minWidth: 60,
+          fixedWidth: true,
+          fixedHeight: false,
+        }),
         columnCount,
+        columnKeyList,
         columnPropsList,
         data,
+        filterable,
+        filterList,
+        filterData,
         getColumnWidth,
-        getHeaderRowWidth,
         getStickyColumnWidth,
         headerList,
         headerKeyList,
         headerRowCount,
         height,
         onChange,
+        onChangeFilterList,
+        overscanColumnCount,
+        readOnly,
         rowCount,
         selectable,
+        setFilterList,
         stickyColumnCount,
         stickyColumnPropsList,
         stickyHeaderList,
         stickyHeaderKeyList,
         stickyHeaderRowCount,
+        stickyColumnKeyList,
         stickyWidth,
       }}
     >
       <div className={defaultGridClassName}>
         <ScrollSync>
-          {({
-            clientHeight,
-            clientWidth,
-            onScroll,
-            scrollHeight,
-            scrollLeft,
-            scrollTop,
-            scrollWidth,
-          }) => (
+          {({ onScroll, scrollLeft, scrollTop }) => (
             <>
-              <div className="flex flex-col" height={height}>
+              <div className="flex flex-col">
                 <StickyGridHeader />
                 <StickyGridBody scrollTop={scrollTop} />
               </div>
               <AutoSizer disableHeight>
                 {({ width }) => {
                   if (!width) return null; // first render will return 0 and store cache;
-                  const bodyWidth = width;
+                  const bodyWidth = width - stickyWidth;
                   return (
                     <>
-                      <GridHeader
-                        width={bodyWidth}
-                        // filterable={filterable}
-                        // filterList={filterList}
-                        // setFilterList={setFilterList}
-                        // headerStyleList={headerStyleList}
-                        scrollLeft={scrollLeft}
-                      />
+                      <GridHeader width={bodyWidth} scrollLeft={scrollLeft} />
                       <GridBody
-                        // data={filterData}
                         width={bodyWidth}
                         onScroll={onScroll}
                         scrollLeft={scrollLeft}
                         scrollTop={scrollTop}
-                        // activeCell={activeCell}
-                        // setActiveCell={setActiveCell}
-                        // onChangeValue={onChangeValue}
                       />
                     </>
                   );
@@ -126,5 +171,31 @@ function Grid({
     </GridProvider>
   );
 }
+
+Grid.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.shape({})),
+  filterable: PropTypes.bool,
+  headerList: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.shape({})))
+    .isRequired,
+  headerKeyList: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.string))
+    .isRequired,
+  height: PropTypes.number,
+  onChange: PropTypes.func,
+  overscanColumnCount: PropTypes.number,
+  selectable: PropTypes.bool,
+  stickyHeaderList: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.shape({}))),
+  stickyHeaderKeyList: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.string)),
+};
+
+Grid.defaultProps = {
+  data: [],
+  filterable: true,
+  height: 500,
+  onChange: null,
+  overscanColumnCount: 10,
+  selectable: false,
+  stickyHeaderList: [[]],
+  stickyHeaderKeyList: [[]],
+};
 
 export default Grid;
