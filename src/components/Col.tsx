b335c39b475fee1f6ad99079b4663d1cef20c431
JY <jherkyesse@gmail.com>
2021-04-29 18:58:43 +0800

    Update Col.tsx

--- src/components/Col.tsx
+++ src/components/Col.tsx
@@ -1,41 +1,59 @@
-
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const defaultClassName = 'flex flex-col';
+const defaultClassName = 'flex flex-col p-1';
 
 type ColProps = {
   alignItems?: 'start' | 'end' | 'center';
   justifyContent?: 'start' | 'end' | 'center' | 'between';
   children?: React.ReactNode;
   className?: string;
-  xs?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 'none';
   sm?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 'none';
   md?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 'none';
   lg?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 'none';
 };
 
 function Col({
-  alignItems = 'start',
+  alignItems,
   children,
-  className = '',
-  justifyContent = 'start',
-  xs = 12,
-  sm = 6,
-  md = 4,
-  lg = 3,
+  className,
+  justifyContent,
+  sm,
+  md,
+  lg,
   ...props
 }: ColProps) {
-  const alitnItemsClassName = `items-${alignItems}`;
+  const alignItemsClassName = `items-${alignItems}`;
   const justifyContentClassName = `justify-${justifyContent}`;
-  const gridColsClassName = `xs:w-${xs}/12 sm:w-${sm}/12 md:w-${md}/12 lg:w-${lg}/12`;
+  const gridColsClassName = `w-full sm:w-${sm}/12 md:w-${md}/12 lg:w-${lg}/12`;
   return (
     <div
-      className={`${defaultClassName} ${alitnItemsClassName} ${justifyContentClassName} ${gridColsClassName} ${className}`}
+      className={`${defaultClassName} ${alignItemsClassName} ${justifyContentClassName} ${gridColsClassName} ${className}`}
       {...props}
     >
       {children}
     </div>
   );
 }
+
+Col.propTypes = {
+  alignItems: PropTypes.string,
+  children: PropTypes.node,
+  className: PropTypes.string,
+  justifyContent: PropTypes.string,
+  sm: PropTypes.number,
+  md: PropTypes.number,
+  lg: PropTypes.number,
+};
+
+Col.defaultProps = {
+  alignItems: 'start',
+  children: null,
+  className: '',
+  justifyContent: 'start',
+  sm: 12,
+  md: 12,
+  lg: 12,
+};
 
 export default Col;
